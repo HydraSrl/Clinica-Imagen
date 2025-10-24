c9f869b8ca915a36e3670a71959fd792fb806249
HydraSrl <hydra.srl.utu@gmail.com>
2025-10-24 17:21:49 -0300

    tratamientos y landing page cambios

--- src/front/pages/tratamientos.php
+++ src/front/pages/tratamientos.php
@@ -3,8 +3,8 @@ require_once '../back/get_tratamientos.php';
 $tratamientos = getAllTratamientos();
 include 'includes/header.php';
 ?>
-<p></p>
 <main id="Tratsweb">
+    <img class="background-img" src="img/shedrack-salami-Aeg3jIg0xsQ-unsplash.jpg" alt="">
     <div class="div-text-img-tratamientos">
         <h1 class="text1-tratamientos-web">Nuestros Tratamientos</h1>
         <h2 class="text2-tratamientos-web">Descubre la variedad de soluciones que ofrecemos para cuidar de tu salud bucal. En Clínica Imagen, nos especializamos en tratamientos de vanguardia para garantizarte una sonrisa sana y radiante.</h2>
